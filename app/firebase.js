9cf636ed84d92800144a50b6334a2ffca828cff7
Jonathan Soo <jonnie@Jonathans-MacBook-Air-3.local>
2025-04-25 22:23:37 -0700

    Adding firbase firestore'

---
+++ app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAerTcCUSfpTGJLeqz-DVnIxmIRtYfy8ag",
+  authDomain: "hacktech25brjk.firebaseapp.com",
+  projectId: "hacktech25brjk",
+  storageBucket: "hacktech25brjk.firebasestorage.app",
+  messagingSenderId: "260317907063",
+  appId: "1:260317907063:web:46de9d9fbcd1df8025de72",
+  measurementId: "G-Y03PZ06G30"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
