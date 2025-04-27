9c1886778fc56ffe1fda7ced15b1b1f7ebe425d3
Jonathan Soo <jonnie@Jonathans-MacBook-Air-3.local>
2025-04-27 03:57:17 -0700

    Hello World'

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -38,7 +38,7 @@ export default function Dashboard() {
 
   const startRecording = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:0000/start-recording");
+      const response = await fetch("http://127.0.0.1:8004/start-recording");
       if (response.ok) {
         setIsRecording(true);
         const data = await response.json();
